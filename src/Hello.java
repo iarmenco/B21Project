7de8bd999ebc03b71176b1bfbae27e35e4d5108e
Dorin <iarmencodorin@gmail.com>
2020-11-10 11:49:27 -0600

    addded new java class

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Happy Birthday silvicica! i wish you all the best");
+        System.out.println("i love you so much");
     }
 
 
